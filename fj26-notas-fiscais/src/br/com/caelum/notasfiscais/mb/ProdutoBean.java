fde4db38bf3e913d605696038ba4d1f537c8009e
Marcos Barbosa <malexandrebarbo@gmail.com>
2015-08-22 16:39:32 -0300

    Inserindo a listagem e o remove

--- fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -1,5 +1,7 @@
 package br.com.caelum.notasfiscais.mb;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 import br.com.caelum.notasfiscais.dao.ProdutoDao;
@@ -10,14 +12,31 @@ public class ProdutoBean {
 	
 	private Produto produto = new Produto();
 	
+	private List<Produto> produtos;
+	
 	public Produto getProduto() {
 		return this.produto;
 	}
 	
-	public void grava() {
+	public String grava() {
 		ProdutoDao dao = new ProdutoDao();
 		dao.adiciona(produto);
-		this.produto = new Produto();
+		
+		return "/produto.xhtml?faces-redirect=true";
+	}
+	
+	public List<Produto> getProdutos() {
+		if(produtos == null) {
+			System.out.println("Carregando produtos...");
+			produtos = new ProdutoDao().listaTodos();
+		}
+		return produtos;
+	}
+	
+	public String remove(Produto produto) {
+		ProdutoDao dao = new ProdutoDao();
+		dao.remove(produto);
+		return "/produto.xhtml?faces-redirect=true";
 	}
 
 }
