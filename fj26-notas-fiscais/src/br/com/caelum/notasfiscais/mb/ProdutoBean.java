6da61e4b98dd15aed32cbe623e817b7f6b5c61f1
Marcos Barbosa <malexandrebarbo@gmail.com>
2015-08-29 11:53:15 -0300

    Adicionando o Altera, e o Ajax para remover.

--- fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -3,11 +3,13 @@ package br.com.caelum.notasfiscais.mb;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.notasfiscais.dao.ProdutoDao;
 import br.com.caelum.notasfiscais.modelo.Produto;
 
 @ManagedBean
+@ViewScoped
 public class ProdutoBean {
 	
 	private Produto produto = new Produto();
@@ -18,11 +20,22 @@ public class ProdutoBean {
 		return this.produto;
 	}
 	
-	public String grava() {
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	public void grava() {
 		ProdutoDao dao = new ProdutoDao();
-		dao.adiciona(produto);
+		//dao.adiciona(produto);
+		if (produto.getId() == null) {
+			dao.adiciona(produto);
+		} else {
+			dao.atualiza(produto);
+		}
+		produtos = dao.listaTodos();
+		this.produto = new Produto();
 		
-		return "/produto.xhtml?faces-redirect=true";
+		//return "/produto.xhtml?faces-redirect=true";
 	}
 	
 	public List<Produto> getProdutos() {
